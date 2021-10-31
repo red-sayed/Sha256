a37d15cffaade6bbd5fde988943eec5b9fc82ef2
Vladimir Rogozin <vladimir20040609@gmail.com>
2021-10-31 12:17:30 +0300

    Initialization

---
+++ Sha256.pro
@@ -0,0 +1,11 @@
+TEMPLATE = app
+CONFIG += console c++11
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += \
+        Sha256/Sha256.cpp \
+        main.cpp
+
+HEADERS += \
+    Sha256/Sha256.h
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+#include "Sha256/Sha256.h"
+
+int main() {
+    std::string inp;
+
+    while (true) {
+        system("clear");
+        std::cout << "\033[4mEnter a message.\033[0m" << std::endl << std::endl << "[\033[93mSha256\033[0m] > ";
+
+        std::cin.ignore();
+        inp.clear();
+        getline(std::cin, inp);
+        std::cout << std::endl;
+
+        std::cout << "[\033[93mSha256\033[0m] \033[4mEncrypted message:\033[0m" << std::endl << std::endl << Red::Sha256Hash(inp);
+        std::cout << std::endl << std::endl << std::endl << "Press Enter to continue.";
+
+        std::cin.get();
+    }
+}
